d837268b97b012ef9187cbf282a9ec2da770ae80
adrian-untu <79197593+adrian-untu@users.noreply.github.com>
2022-02-21 18:42:13 +0200

    Am terminat partea compulsory
    Implementare cu succes

---
+++ Laboratorul 1/src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");//pentru prima parte
+        String languages[] = new String[]  {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};//pentru a doua parte
+        int n = (int) (Math.random() * 1_000_000);
+        n = n * 3;
+        String binaryString="10101";
+        int binar=Integer.parseInt(binaryString,2);
+        n = n + binar;
+        String hex="FF";
+        int hexa=Integer.parseInt(hex,16);
+        n = n + hexa;
+        n = n * 6;
+        System.out.println(n);
+        int s = 0;
+        while(n!=0) {
+            s = s + n % 10;
+            n = n / 10;
+        }
+        System.out.println(s);
+        int k = 0;
+        if ( s > 9)
+            do {
+                k = s;
+                s = 0;
+                while(k!=0) {
+                    s = s + k % 10;
+                    k /= 10;
+                }
+            } while ( s > 9 );
+        System.out.println(s);
+        //pentru a treia parte
+        System.out.println("Willy-nilly, this semester I will learn " + languages[s]);//pentru a patra parte
+    }
+}
