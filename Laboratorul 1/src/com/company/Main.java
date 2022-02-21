dc17b9d9c82061a3fd98505f5378b78e3301498a
adrian-untu <79197593+adrian-untu@users.noreply.github.com>
2022-02-21 19:10:32 +0200

    Versiunea finala

--- Laboratorul 1/src/com/company/Main.java
+++ Laboratorul 1/src/com/company/Main.java
@@ -1,36 +1,25 @@
 package com.company;
 
 public class Main {
+    public static int getControlDigit (int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n % 9 == 0) {
+            return 9;
+        }
+        return n % 10;
+    }
+
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");//pentru prima parte
-        String languages[] = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};//pentru a doua parte
+        String[] languages = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};//pentru a doua parte
         int n = (int) (Math.random() * 1_000_000);
-        n = n * 3;
-        String binaryString="10101";
-        int binar=Integer.parseInt(binaryString,2);
-        n = n + binar;
-        String hex="FF";
-        int hexa=Integer.parseInt(hex,16);
-        n = n + hexa;
-        n = n * 6;
+        n = (n * 3 + 0b10101 + 0xFF) * 6;
         System.out.println("n este " + n);
-        int s = 0;
-        while(n!=0) {
-            s = s + n % 10;
-            n = n / 10;
-        }
-        int k = 0;
-        if ( s > 9)
-            do {
-                k = s;
-                s = 0;
-                while(k!=0) {
-                    s = s + k % 10;
-                    k /= 10;
-                }
-            } while ( s > 9 );
+       int result = getControlDigit(n);
         //pentru a treia parte
-        System.out.println("Willy-nilly, this semester I will learn " + languages[s]);//pentru a patra parte
+        System.out.println("Willy-nilly, this semester I will learn " + languages[result]);//pentru a patra parte
     }
 }
