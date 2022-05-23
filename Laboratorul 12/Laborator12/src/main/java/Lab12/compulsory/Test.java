360fc7ba92fc08616f02f9b1dea2483fedfbdad8
adrian-untu <79197593+adrian-untu@users.noreply.github.com>
2022-05-23 19:35:33 +0300

    Compulsory lab 12

---
+++ Laboratorul 12/Laborator12/src/main/java/Lab12/compulsory/Test.java
@@ -0,0 +1,13 @@
+package Lab12.compulsory;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Test
+{
+}
